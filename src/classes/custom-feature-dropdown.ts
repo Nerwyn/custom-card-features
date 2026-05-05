82783332dbf256c4a40f6e7399eb99f2ee534f07
Nerwyn Singh <nerwyn1994@gmail.com>
2026-05-05 00:07:52 -0400

    update dropdown box shadow

--- src/classes/custom-feature-dropdown.ts
+++ src/classes/custom-feature-dropdown.ts
@@ -339,10 +339,7 @@ export class CustomFeatureDropdown extends BaseCustomFeature {
 						opacity 0.03s linear,
 						transform 0.12s cubic-bezier(0, 0, 0.2, 1),
 						height 250ms cubic-bezier(0, 0, 0.2, 1);
-					box-shadow:
-						0px 5px 5px -3px rgba(0, 0, 0, 0.2),
-						0px 8px 10px 1px rgba(0, 0, 0, 0.14),
-						0px 3px 14px 2px rgba(0, 0, 0, 0.12);
+					box-shadow: var(--wa-shadow-m);
 				}
 				.collapsed {
 					height: 0;
